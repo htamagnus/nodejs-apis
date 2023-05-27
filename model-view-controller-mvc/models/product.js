5a3af683bd70ed075be84338faef618a1dafe20d
Ágatha <agathamagnusg@hotmail.com>
2023-05-27 11:03:56 -0300

    storing data in files via the model and fetching data from files via the model

--- model-view-controller-mvc/models/product.js
+++ model-view-controller-mvc/models/product.js
@@ -1,13 +1,38 @@
-const products = [];
-
+const fs = require("fs");
+const path = require("path");
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
-    }
-    save() {
+  constructor(t) {
+    this.title = t;
+  }
+  save() {
+    const p = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, fileContent) => {
+        let products = [];
+        if (!err) {
+            products = JSON.parse(fileContent);
+        }
         products.push(this);
-    }
-    static fetchAll() {
-        return products;
-    }
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+            console.log(err);
+        });
+    });
+  }
+  static fetchAll(cb) {
+    const p = path.join(
+        path.dirname(require.main.filename),
+        "data",
+        "products.json"
+      );
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return cb([]);
+        }
+        cb(JSON.parse(fileContent));
+    })
+  }
 };
+
