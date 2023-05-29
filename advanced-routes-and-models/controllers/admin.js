74caf8ecfefc7a9d74d16393f64e130d57fa2e49
Ágatha <agathamagnusg@hotmail.com>
2023-05-29 08:03:48 -0300

    pre-propulating the edit product page with data

--- advanced-routes-and-models/controllers/admin.js
+++ advanced-routes-and-models/controllers/admin.js
@@ -4,14 +4,15 @@ exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
+    editing: false,
   });
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const title = req.body.title; 
+  const title = req.body.title;
   const imageUrl = req.body.imageUrl;
-  const description = req.body.description;
   const price = req.body.price;
+  const description = req.body.description;
   const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect("/");
@@ -19,11 +20,20 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
-  if (!editMode) return res.redirect("/");
-  res.render("admin/edit-product", {
-    pageTitle: "Edit Product",
-    path: "/admin/edit-product",
-    editing: editMode
+  if (!editMode) {
+    return res.redirect("/");
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.redirect("/");
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product,
+    });
   });
 };
 
