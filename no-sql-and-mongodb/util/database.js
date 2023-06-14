28e66fafaf05577ed253d76fc2aa0a4f5498c4eb
hta <agathamagnusg@hotmail.com>
2023-06-14 09:52:32 -0300

    installing the mongoDB driver

--- no-sql-and-mongodb/util/database.js
+++ no-sql-and-mongodb/util/database.js
@@ -1,8 +1,18 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', '1234', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://root:1234@cluster0.1ix7dxb.mongodb.net/?retryWrites=true&w=majority"
+  )
+    .then(client => {
+      console.log("Connected!");
+      callback(client);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
 
-module.exports = sequelize;
+module.exports = mongoConnect;
+
