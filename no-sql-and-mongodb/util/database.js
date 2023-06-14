1f9878c307bc4696875f4e42c306f58899105390
hta <agathamagnusg@hotmail.com>
2023-06-14 10:05:29 -0300

    finishing the database connection

--- no-sql-and-mongodb/util/database.js
+++ no-sql-and-mongodb/util/database.js
@@ -1,18 +1,30 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    "mongodb+srv://root:1234@cluster0.1ix7dxb.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://root:1234@cluster0.1ix7dxb.mongodb.net/shop?retryWrites=true&w=majority"
   )
     .then(client => {
       console.log("Connected!");
-      callback(client);
+      _db = client.db();
+      callback();
     })
     .catch(err => {
       console.log(err);
+      throw err;
     });
 }
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
 
