c8ec2a0d05d7de70433773e41b8cae383c409c76
Ágatha <agathamagnusg@hotmail.com>
2023-06-19 09:07:36 -0300

    fecthing a single product

--- no-sql-and-mongodb/models/product.js
+++ no-sql-and-mongodb/models/product.js
@@ -1,3 +1,5 @@
+mongodb = require("mongodb");
+
 const getDb = require("../util/database").getDb;
 
 class Product {
@@ -33,6 +35,21 @@ class Product {
       .catch((err) => {
         console.log(err);
       });
+  }
+
+  static findByPk(prodId) {
+    const db = getDb();
+    return db
+    .collection('products')
+    .find({_id: new mongodb.ObjectId (prodId) })
+    .next()
+    .then(product => {
+      console.log(product);
+      return product;
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }
 }
 
