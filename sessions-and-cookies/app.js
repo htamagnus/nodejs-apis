a995ed489743e3c8edfe8265e049deffda7d9bb0
Ágatha <agathamagnusg@hotmail.com>
2023-10-09 07:33:44 -0300

    creating the login form

--- sessions-and-cookies/app.js
+++ sessions-and-cookies/app.js
@@ -14,6 +14,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -29,19 +30,20 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+    'mongodb+srv://hta:agatha@cluster0.r2eb6bu.mongodb.net/shop?retryWrites=true&w=majority'
   )
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
         const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
+          name: 'Hta',
+          email: 'hta@test.com',
           cart: {
             items: []
           }
@@ -49,7 +51,7 @@ mongoose
         user.save();
       }
     });
-    app.listen(3000);
+    app.listen(3001);
   })
   .catch(err => {
     console.log(err);
