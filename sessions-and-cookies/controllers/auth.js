42cc6785cf235d6c588494a9429ba784b2e58609
Ágatha <agathamagnusg@hotmail.com>
2023-11-20 15:38:17 -0300

    deleting a cookie

--- sessions-and-cookies/controllers/auth.js
+++ sessions-and-cookies/controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
   // const isLoggedIn = req.get("Cookie").split(";")[1].trim().split("=")[1];
   res.render("auth/login", {
@@ -8,6 +10,17 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  res.session.isLoggedIn = true;
-  res.redirect("/");
+  User.findById('5bab316ce0a7c75f783cb8a8')
+  .then(user => {
+    res.session.isLoggedIn = true;
+    req.session.user = user;
+    res.redirect("/");
+  })
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(() => {
+    res.redirect("/");
+  });
+
 };
