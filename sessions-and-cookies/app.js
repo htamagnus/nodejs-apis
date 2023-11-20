214172ceb2fe45612ff3bf3356aec83ffd4c6c9b
Ágatha <agathamagnusg@hotmail.com>
2023-11-20 16:11:09 -0300

    making add to cart work again

--- sessions-and-cookies/app.js
+++ sessions-and-cookies/app.js
@@ -29,6 +29,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({secret: 'my secret', resave: false, saveUninitialized: false, store: store}));
 
 app.use((req, res, next) => {
+  if(!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+  .then(user => {
+    req.user = user;
+    next(); 
+   })
+})
+
+app.use((req, res, next) => {
   User.findById('5bab316ce0a7c75f783cb8a8')
     .then(user => {
       req.user = user;
