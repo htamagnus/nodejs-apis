54f6de52efb7eb0c50912c282c8ea75123b23724
Ágatha <agathamagnusg@hotmail.com>
2023-12-29 11:52:52 -0300

    adding additional flash messages

--- authentication/controllers/auth.js
+++ authentication/controllers/auth.js
@@ -2,6 +2,12 @@ const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0]
+  } else {
+    message = null
+  }
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
