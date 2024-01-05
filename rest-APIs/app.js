9baccaf0e20b8445b6a4b748ecbaa6a0fbee5ee6
Ágatha <agathamagnusg@hotmail.com>
2024-01-05 10:27:11 -0300

    setting up a post model

--- rest-APIs/app.js
+++ rest-APIs/app.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const feedRoutes = require('./routes/feed');
+const feedRoutes = require("./routes/feed");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -9,12 +10,20 @@ const app = express();
 app.use(bodyParser.json()); // application/json
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // * = all domains
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // which headers are allowed
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*"); // * = all domains
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // which headers are allowed
+  next();
 });
 
-app.use('/feed', feedRoutes);
+app.use("/feed", feedRoutes);
 
-app.listen(8080);
+mongoose
+  .connect("mongodb+srv://hta:agatha@cluster0.r2eb6bu.mongodb.net/shop")
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
