0ec6484a6ed4c170a231bfede45c9a0ec8b4cbed
Ágatha <agathamagnusg@hotmail.com>
2024-01-05 10:33:00 -0300

    static images and error handling

--- rest-APIs/controllers/feed.js
+++ rest-APIs/controllers/feed.js
@@ -1,48 +1,51 @@
-const { validationResult } = require("express-validator");
+const { validationResult } = require('express-validator/check');
 
-const Post = require("../models/post");
+const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
       {
-        _id: "1",
-        title: "First Post",
-        content: "This is the first post!",
-        imageUrl: "images/duck.jpg",
+        _id: '1',
+        title: 'First Post',
+        content: 'This is the first post!',
+        imageUrl: 'images/duck.jpg',
         creator: {
-          name: "Hta",
+          name: 'Maximilian'
         },
-        createdAt: new Date(),
-      },
-    ],
+        createdAt: new Date()
+      }
+    ]
   });
 };
 
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422).json({
-      message: "Validation failed, entered data is incorrect.",
-      errors: errors.array(),
-    });
+    const error = new Error('Validation failed, entered data is incorrect.');
+    error.statusCode = 422;
+    throw error;
   }
   const title = req.body.title;
   const content = req.body.content;
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: "images/duck.jpg",
-    creator: { name: "Hta" },
+    imageUrl: 'images/duck.jpg',
+    creator: { name: 'Maximilian' }
   });
   post
     .save()
-    .then((result) => {
-      // Create post in db
+    .then(result => {
       res.status(201).json({
-        message: "Post created successfully!",
+        message: 'Post created successfully!',
         post: result
       });
     })
-    .catch((err) => console.log(err));
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
