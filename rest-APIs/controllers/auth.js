1c83812a90e147e00b4b476dd79d6049c576b101
Ágatha <agathamagnusg@hotmail.com>
2024-01-05 10:54:31 -0300

    signIn users up

--- rest-APIs/controllers/auth.js
+++ rest-APIs/controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 exports.signup = (req, res, next) => {
     const errors = validationResult(req);
@@ -12,4 +13,15 @@ exports.signup = (req, res, next) => {
     const email = req.body.email;
     const name = req.body.name;
     const password = req.body.password;
+    bcrypt.hash(password, 12).then(hashedPw => {
+        const user = new User({
+            email: email,
+            password: hashedPw,
+            name: name
+        });
+        return user.save();
+    })
+    .then(result => {
+        res.status(201).json({ message: 'User created!', userId: result._id });
+    })
 }
