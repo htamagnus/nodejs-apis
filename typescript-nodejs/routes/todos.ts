f715912439b4ab0c367ed03a6c9254dc80bb9626
Ágatha <agathamagnusg@hotmail.com>
2024-01-06 14:22:22 -0300

    using type casting

--- typescript-nodejs/routes/todos.ts
+++ typescript-nodejs/routes/todos.ts
@@ -3,6 +3,9 @@ import { Todo } from '../models/todo';
 
 let todos: Todo[]  = [];
 
+type RequestBody = { text: string };
+type RequestParams = { todoId: string };
+
 const router = Router();
 
 router.get('/', (req, res, next) => {
@@ -10,9 +13,10 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/todo', (req, res, next) => {
+    const body = req.body as RequestBody;
     const newTodo: Todo = {
         id: new Date().toISOString(),
-        text: req.body.text
+        text: body.text
     };
 
     todos.push(newTodo);
@@ -21,11 +25,13 @@ router.post('/todo', (req, res, next) => {
 });
 
 router.put('/todo/:todoId', (req, res, next) => {
-    const todoId = req.params.todoId;
+    const params = req.params as RequestParams;
+    const todoId = params.todoId;
+    const body = req.body as RequestBody
     const todoIndex = todos.findIndex(todoItem => todoItem.id === todoId);
 
     if (todoIndex >= 0) {
-        todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+        todos[todoIndex] = { id: todos[todoIndex].id, text: body.text };
         return res.status(200).json({ message: 'Updated todo', todos: todos });
     }
 
@@ -33,7 +39,8 @@ router.put('/todo/:todoId', (req, res, next) => {
 });
 
 router.delete('/todo/:todoId', (req, res, next) => {
-    todos = todos.filter(todoItem => todoItem.id !== req.params.todoId);
+    const params = req.params as RequestParams;
+    todos = todos.filter(todoItem => todoItem.id !== params.todoId);
     res.status(200).json({ message: 'Deleted todo', todos: todos });
 });
 
